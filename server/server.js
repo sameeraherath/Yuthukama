395b38d6e621bf9832eebb73b6c28c799bb89001
sameeraherath <sameerastar6@gmail.com>
2025-03-06 16:34:46 +0530

    Fix bug

--- server/server.js
+++ server/server.js
@@ -13,7 +13,12 @@ connectDb();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
