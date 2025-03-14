1de6221aff79348f9ae6613d55d8ea0992757e8d
sameeraherath <sameerastar6@gmail.com>
2025-03-14 08:12:23 +0530

    Implement PostCard

---
+++ client/src/components/PostCard.jsx
@@ -0,0 +1,39 @@
+import {
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from "@mui/material";
+
+const PostCard = ({ post }) => {
+  return (
+    <Card key={post._id} sx={{ maxWidth: 420, borderRadius: 5 }}>
+      <CardMedia
+        component="img"
+        height="200"
+        image={post.image}
+        alt={post.title}
+      />
+      <CardContent>
+        <Typography variant="h6" gutterBottom>
+          {post.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {post.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" color="primary">
+          Like
+        </Button>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default PostCard;
--- client/src/pages/HomeScreen.jsx
+++ client/src/pages/HomeScreen.jsx
@@ -1,15 +1,8 @@
 import { useEffect, useState } from "react";
-import {
-  Box,
-  Card,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActions,
-  Button,
-} from "@mui/material";
+import { Box } from "@mui/material";
 import { useAuth } from "../context/useAuth";
 import { useNavigate } from "react-router-dom";
+import PostCard from "../components/PostCard";
 
 const HomeScreen = () => {
   const { user } = useAuth();
@@ -23,10 +16,15 @@ const HomeScreen = () => {
   }, [user, navigate]);
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchPost = async () => {
       try {
         const response = await fetch(
-          `${import.meta.env.VITE_SERVER_URL}/api/posts`
+          `${import.meta.env.VITE_SERVER_URL}/api/posts`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+          }
         );
         if (!response.ok) {
           throw new Error("Failed to fetch posts");
@@ -39,53 +37,28 @@ const HomeScreen = () => {
       }
     };
 
-    fetchPosts();
+    fetchPost();
   }, []);
 
   return (
-    <>
-      <Box
-        sx={{
-          display: "grid",
-          gridTemplateColumns: {
-            xs: "1fr",
-            sm: "1fr 1fr",
-            md: "repeat(3, 1fr)",
-            lg: "repeat(3, 1fr)",
-          },
-          gap: 3,
-          p: 3,
-          px: { lg: 10 },
-        }}
-      >
-        {posts.map((post) => (
-          <Card key={post._id} sx={{ maxWidth: 420, borderRadius: 5 }}>
-            <CardMedia
-              component="img"
-              height="200"
-              image={post.image}
-              alt={post.title}
-            />
-            <CardContent>
-              <Typography variant="h6" gutterBottom>
-                {post.title}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {post.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary">
-                Like
-              </Button>
-              <Button size="small" color="primary">
-                Share
-              </Button>
-            </CardActions>
-          </Card>
-        ))}
-      </Box>
-    </>
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: {
+          xs: "1fr",
+          sm: "1fr 1fr",
+          md: "repeat(3, 1fr)",
+          lg: "repeat(3, 1fr)",
+        },
+        gap: 3,
+        p: 3,
+        px: { lg: 10 },
+      }}
+    >
+      {posts.map((post) => {
+        return <PostCard key={post._id} post={post} />;
+      })}
+    </Box>
   );
 };
 
