f8d33426cef077bdc0f502f12fd4167b2e17550f
sameeraherath <sameerastar6@gmail.com>
2025-03-15 20:18:30 +0530

    Add post creation function

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,36 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
   IconButton,
   Typography,
-  Button,
   Box,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Button,
 } from "@mui/material";
 import PostAddIcon from "@mui/icons-material/PostAdd";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import LogoutIcon from "@mui/icons-material/Logout";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const [postContent, setPostContent] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handlePostSubmit = () => {
+    console.log("Post Content: ", postContent);
+    setPostContent("");
+    handleClose();
+  };
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Yuthukama
-        </Typography>
-        <Box sx={{ display: "flex", alignItems: "center" }}>
-          <IconButton color="inherit">
-            <PostAddIcon />
-          </IconButton>
-          <IconButton color="inherit">
-            <AccountCircleIcon />
-          </IconButton>
-          <IconButton color="inherit">
-            <LogoutIcon />
-          </IconButton>
-        </Box>
-      </Toolbar>
-    </AppBar>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Yuthukama
+          </Typography>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <IconButton color="inherit" onClick={handleClickOpen}>
+              <PostAddIcon />
+            </IconButton>
+            <IconButton color="inherit">
+              <AccountCircleIcon />
+            </IconButton>
+            <IconButton color="inherit">
+              <LogoutIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>Create a New Post</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="postContent"
+            label="Post Content"
+            type="text"
+            fullWidth
+            variant="outlined"
+            value={postContent}
+            onChange={(e) => setPostContent(e.target.value)}
+            multiline
+            rows={4}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handlePostSubmit} color="primary">
+            Post
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
