967e52ee389172527746425fc321a5065304e2e2
sameeraherath <sameerastar6@gmail.com>
2025-05-03 09:38:51 +0530

    feat: implement post deletion functionality with confirmation dialog in ProfilePage

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -3,11 +3,13 @@ import {
   getPosts,
   createPost,
   getUserPosts,
+  deletePost,
 } from "../controllers/postController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 router.route("/").get(protect, getPosts).post(protect, createPost);
 router.route("/user/:userId").get(protect, getUserPosts);
+router.route("/:id").delete(protect, deletePost);
 
 export default router;
