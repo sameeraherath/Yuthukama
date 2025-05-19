03ef547ac665268b851926f594cbf13d4bd6b294
sameeraherath <sameerastar6@gmail.com>
2025-05-19 18:49:39 +0530

    feat: add update username route to userRoutes
    - Imported updateUsername controller.
    - Added PUT route for updating user username, protected by authentication middleware.

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { getUserProfile } from "../controllers/userController.js";
+import {
+  getUserProfile,
+  updateUsername,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/profile", protect, getUserProfile);
+router.put("/username", protect, updateUsername);
 
 export default router;
